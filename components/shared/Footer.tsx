afbe40420c2b13fd108d19ea8a243aa5da5f10d6
Astride <astridesmn@gmail.com>
2024-01-22 17:17:11 +1100

    final

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+          <Image 
+            src="/assets/images/epacific.png"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2023 ePacific. All Rights reserved.</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
