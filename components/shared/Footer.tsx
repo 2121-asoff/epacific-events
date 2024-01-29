cc260eb99b4437b5e8f0c23d34f645145327e379
Astride <astridesmn@gmail.com>
2024-01-29 20:24:16 +1100

    first commit

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -14,7 +14,7 @@ const Footer = () => {
           />
         </Link>
 
-        <p>2023 ePacific. All Rights reserved.</p>
+        <p>© 2024 ePacific. All Rights reserved.</p>
       </div>
     </footer>
   )
