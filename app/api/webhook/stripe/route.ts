c38db6d5ebd26bcd3abecbb0157cdd147677c472
Astride <astridesmn@gmail.com>
2024-01-30 12:34:05 +1100

    final

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,6 +1,5 @@
 import stripe from 'stripe'
 import { NextResponse } from 'next/server'
-import { createOrder } from '@/lib/actions/order.actions'
 
 export async function POST(request: Request) {
   const body = await request.text()
@@ -31,8 +30,8 @@ export async function POST(request: Request) {
       createdAt: new Date(),
     }
 
-    const newOrder = await createOrder(order)
-    return NextResponse.json({ message: 'OK', order: newOrder })
+
+    return NextResponse.json({ message: 'OK' })
   }
 
   return new Response('', { status: 200 })
